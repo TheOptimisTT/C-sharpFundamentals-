28f4b118e98da121d212871472c198637ec561d7
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-11 11:02:51 +0200

    solution 2

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/2.Passed/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Passed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            double grade = double.Parse(Console.ReadLine());
+
+            //logic
+            if (grade >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
