27b443cff96e69b287b4315809752c30e534f946
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-11 11:08:53 +0200

    Solution 9

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _09._Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Var
+            int c = 1;
+            int sum = 0;
+
+            //Input
+            int number = int.Parse(Console.ReadLine());
+
+            //Logic
+            for (int i = 1; i <= number; i++)
+            {
+                c = (i * 2) - 1;
+                Console.WriteLine(c);
+                sum += c;        
+            }
+
+            //Output
+            Console.WriteLine("Sum: {0}",sum);
+        }
+    }
+}
