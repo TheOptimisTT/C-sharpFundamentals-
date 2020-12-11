57cafd1b36997d7445d97a31224076d0e89c2f38
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-11 11:10:16 +0200

    Solution 12

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/12. Even Number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _12._Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var
+            var a= 0;
+
+            //Logic
+            while (true)
+            {
+                //Input
+                int number = int.Parse(Console.ReadLine());
+
+                //Output if not even
+                if (number % 2 != 0)
+                {
+                    
+                    Console.WriteLine("Please write an even number.");
+                }
+                else
+                {
+                    a = number;
+                    break;
+                }
+            }
+
+            //Output
+            Console.WriteLine("The number is: {0}",Math.Abs(a));
+        }
+    }
+}
