05c7c3f8e4771afc6209f16ebf6ab8bee14bd37b
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-11 11:04:12 +0200

    Solution 4

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/4.Back In 30 Minutes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const
+            int waitingTime = 30;
+
+            //input
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            //logic
+            int sum = (hour * 60) + minutes + waitingTime;
+            int printHour = sum / 60;
+            int printMinute = sum - (printHour * 60);
+            if (printMinute > 59)
+            {
+                printMinute -= 60;
+                printHour++;
+            }
+            if (printHour > 23)
+            {
+                printHour -= 24;
+            }
+            //Output
+            Console.WriteLine("{0}:{1:D2}", printHour, printMinute);
+
+        }
+    }
+}
