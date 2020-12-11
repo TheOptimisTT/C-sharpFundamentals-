25994ec48ee2a6f0e71c3a8ad0c81a1fa57f9842
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-11 11:03:52 +0200

    Solution 3

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/3.Passed or Failed/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Passed_or_Failed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            double grade = double.Parse(Console.ReadLine());
+            //logic
+            if (grade>=3)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
