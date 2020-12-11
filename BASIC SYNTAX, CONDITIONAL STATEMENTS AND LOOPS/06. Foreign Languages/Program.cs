401f3c6bc0f7bb24c70a0aa9bcfaa81807e37173
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-11 11:05:02 +0200

    Solution 6

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/06. Foreign Languages/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06._Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            string country = Console.ReadLine();
+
+            //logic
+            if (country == "USA"|| country== "England")
+            {
+                Console.WriteLine("English");
+            }
+            else if (country == "Spain" || country == "Argentina" || country == "Mexico")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
