aa36da17436fe18e079a0555a820d46d0cc47ac0
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-11 11:09:46 +0200

    Solution 11

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _10._Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var
+            int a = 0;
+
+            //input
+            int number = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+
+            //Logic
+            if (times<=10)
+            {
+                for (int i = times; i <= 10; i++)
+                {
+                    a = number * i;
+                    Console.WriteLine("{0} X {1} = {2}", number, i, a);
+                }
+            }
+            else
+            {
+                a = number * times;
+                Console.WriteLine("{0} X {1} = {2}", number, times, a);
+            }
+           
+        }
+    }
+}
