81702173d27093817073b71b6d3cceed3544ab74
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-11 11:01:37 +0200

    solution 1

---
+++ BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/1.Student_information/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Student_information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+
+            //output
+            Console.WriteLine("Name: {0}, Age: {1}, Grade: {2:f2}",name,age,grade);
+        }
+    }
+}
