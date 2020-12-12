4f960ae6459854000e3fa183a4ffdf279710d711
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-12 17:27:39 +0200

    Update

---
+++ 6. EXERCISE BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/05. Login/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Var
+            string password = string.Empty;
+            int numberOfAttempts = 0;
+
+            //Input
+            string username = Console.ReadLine();
+
+            //Password
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            //Logic
+            while (true)
+            {
+                numberOfAttempts++;
+                string passwordInput = Console.ReadLine();
+                if (passwordInput == password)
+                {
+                    Console.WriteLine("User {0} logged in.", username);
+                    break;
+                }
+                else if (numberOfAttempts < 4)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                else if (numberOfAttempts >= 4)
+                {
+                    Console.WriteLine("User {0} blocked!", username);
+                    break;
+                }
+            }
+        }
+    }
+}
