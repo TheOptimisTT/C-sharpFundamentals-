f92a9518cc2e0629e40a9f828ffe3225f7a11028
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-12 17:30:38 +0200

    Update

---
+++ 6. More Exercise C# Intro and Basic Syntax/02. English Name of the Last Digit/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _02._English_Name_of_the_Last_Digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var
+            int a = 0;
+
+            //Input
+            int number = int.Parse(Console.ReadLine());
+
+            //Logic
+            a = number % 10; //gets the last number
+
+            switch (a)
+            {
+                case 0:
+                    Console.WriteLine("zero");
+                    break;
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+                case 4:
+                    Console.WriteLine("four");
+                    break;
+                case 5:
+                    Console.WriteLine("five");
+                    break;
+                case 6:
+                    Console.WriteLine("six");
+                    break;
+                case 7:
+                    Console.WriteLine("seven");
+                    break;
+                case 8:
+                    Console.WriteLine("eight");
+                    break;
+                case 9:
+                    Console.WriteLine("nine");
+                    break;
+                
+            }
+        }
+    }
+}
