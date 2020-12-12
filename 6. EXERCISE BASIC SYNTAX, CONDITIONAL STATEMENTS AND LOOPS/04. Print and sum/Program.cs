f63d33daa341f2de103b3742041ab6633a15ff67
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-12 17:27:27 +0200

    Update

---
+++ 6. EXERCISE BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/04. Print and sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Var
+            int sum = 0;
+
+            //Input
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+
+            //Logic
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                Console.Write("{0} ",i);
+                sum += i;
+            }
+
+            //Output
+            Console.WriteLine("");
+            Console.WriteLine("Sum: {0}",sum);
+        }
+    }
+}
