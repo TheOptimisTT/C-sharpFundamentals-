a444e69ef71e3e3ff5f805b590863835416e903a
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-12 17:30:22 +0200

    Update

---
+++ 6. More Exercise C# Intro and Basic Syntax/01. Sort Numbers/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _01._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            //Input
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            //Logic
+            if (a > b && a > c)
+            {
+                if (b > c)
+                {
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                }
+                else
+                {
+                    Console.WriteLine(a);
+                    Console.WriteLine(c);
+                    Console.WriteLine(b);
+                }
+            }
+            else if (b>c&&b>a)
+            {
+                if (c>a)
+                {
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine(b);
+                    Console.WriteLine(a);
+                    Console.WriteLine(c);
+                }
+            }
+            else if (c>a&&c>b)
+            {
+                if (a> b)
+                {
+                    Console.WriteLine(c);
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                    Console.WriteLine(b);
+                    Console.WriteLine(a);
+                }
+
+            }
+
+            
+        }
+    }
+}
