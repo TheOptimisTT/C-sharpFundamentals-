b7125a16785ab6bd9ff18930580ee4ef8651e595
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-12 17:28:57 +0200

    Update

---
+++ 6. EXERCISE BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/08. Triangle of Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _08._Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int number = int.Parse(Console.ReadLine());
+
+            //Logic
+            for (int i = 1; i <= number; i++)
+            {
+                for (int i1 = 0; i1 < i; i1++)
+                {
+                    Console.Write("{0} ",i) ;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
