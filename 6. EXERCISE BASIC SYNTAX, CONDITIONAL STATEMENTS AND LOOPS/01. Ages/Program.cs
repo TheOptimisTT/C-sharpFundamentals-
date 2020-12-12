4566a50ba00b7b10fbaf83b842ca95f1d9dc2137
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-12 17:26:05 +0200

    Update

---
+++ 6. EXERCISE BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/01. Ages/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01._Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int age = int.Parse(Console.ReadLine());
+
+            //Conditions
+            bool con1 = age >= 0 && age <= 2;
+            bool con2 = age > 2 && age <= 13;
+            bool con3 = age > 13 && age <= 19;
+            bool con4 = age > 19 && age <= 65;
+            bool con5 = age >= 66;
+
+            //Logic
+            if (con1)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (con2)
+            {
+                Console.WriteLine("child");
+            }
+            else if (con3)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (con4)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (con5)
+            {
+                Console.WriteLine("elder");
+            }
+        }
+    }
+}
