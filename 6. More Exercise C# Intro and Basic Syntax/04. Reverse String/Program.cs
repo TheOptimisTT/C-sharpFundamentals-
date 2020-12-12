2d5b5c5bf0271a6a876f8cbfb052c22bbb75c49d
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-12 17:31:12 +0200

    Update

---
+++ 6. More Exercise C# Intro and Basic Syntax/04. Reverse String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+
+           string input = Console.ReadLine();
+
+            //Logic
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
+        }
+    }
+}
