2d395186c9c0e618b3ed3048c807a9097de4b48a
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:08:40 +0200

    update

---
+++ 7. DATA TYPES AND VARIABLES/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03._Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal sum = 0;
+            int numberOfNumbers = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfNumbers; i++)
+            {
+                decimal number1 = decimal.Parse(Console.ReadLine());
+                sum += number1;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
