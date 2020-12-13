11e8e9692b9f1c674e895128f0fe38aa4ef7dc61
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:11:47 +0200

    Update

---
+++ 8. EXERCISE DATA TYPES AND VARIABLES/02. Sum Digits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int number = int.Parse(Console.ReadLine());
+
+            while (number>0)
+            {
+                sum += number % 10;
+                number /= 10;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
