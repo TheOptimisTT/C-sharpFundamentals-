798ff5943eb175b3fba58d1d2f536f36dd45b4ec
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:09:38 +0200

    Update

---
+++ 7. DATA TYPES AND VARIABLES/08. Town Info/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08._Town_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            string townName = Console.ReadLine();
+            uint population = uint.Parse(Console.ReadLine());
+            uint area = uint.Parse(Console.ReadLine());
+
+            //output
+            Console.WriteLine("Town {0} has population of {1} and area {2} square km.",townName,population,area);
+        }
+    }
+}
