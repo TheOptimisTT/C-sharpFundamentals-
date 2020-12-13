444a570cd8aa8578c3e0d7a4d6cff21206aa8b86
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:11:11 +0200

    Update

---
+++ 8. EXERCISE DATA TYPES AND VARIABLES/01. Integer Operations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int forthNumber = int.Parse(Console.ReadLine());
+
+            //Var
+            Int64 sum = (firstNumber + secondNumber);
+            sum /= thirdNumber;
+            sum *= forthNumber;
+
+            //Output
+            Console.WriteLine(sum);
+        }
+    }
+}
