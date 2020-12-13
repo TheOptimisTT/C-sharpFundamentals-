7c55cd8903d97f9a88474fc997c4577ffdd5ced9
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:12:10 +0200

    Update

---
+++ 8. EXERCISE DATA TYPES AND VARIABLES/04. Sum of Chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04._Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Var
+            int sumOfTheASCIInumbers = 0;
+
+            //Input
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            //Logic
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                sumOfTheASCIInumbers += letter;
+            }
+
+            //Output
+            Console.WriteLine("The sum equals: {0}",sumOfTheASCIInumbers);
+        }
+    }
+}
