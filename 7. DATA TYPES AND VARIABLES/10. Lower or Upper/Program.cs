df640e48936480fae88855bad53643f0990ee0df
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:10:01 +0200

    Update

---
+++ 7. DATA TYPES AND VARIABLES/10. Lower or Upper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            char input = char.Parse(Console.ReadLine());
+
+            //Logic
+            if (input >= 65 && input <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if (input >= 97 && input <= 122)
+            {
+                Console.WriteLine("lower-case");
+            }
+
+
+            //Using ASCII table ! http://www.asciitable.com/
+        }
+    }
+}
