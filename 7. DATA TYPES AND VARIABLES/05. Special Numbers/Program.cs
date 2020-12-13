af54ac3594bfefad6340bef1f911693eeab0d446
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:09:03 +0200

    Update

---
+++ 7. DATA TYPES AND VARIABLES/05. Special Numbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int input = int.Parse(Console.ReadLine());
+
+            //logic
+            for (int i = 1; i <= input; i++)
+            {
+                if (i <= 9)
+                {
+                    if (i % 5 == 0 || i % 7 == 0|| i % 11 == 0)
+                    {
+                        Console.WriteLine("{0} -> True", i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} -> False", i);
+                    }
+                }
+                else
+                {
+                    int lastNumber = i;
+                    int sum = 0;
+                    while (lastNumber > 0)
+                    {
+                        sum += lastNumber % 10;
+                        lastNumber /= 10;
+                    }
+                    if (sum == 5 || sum == 7 || sum == 11)
+                    {
+                        Console.WriteLine("{0} -> True", i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} -> False", i);
+                    }
+                }
+
+            }
+
+        }
+    }
+}
