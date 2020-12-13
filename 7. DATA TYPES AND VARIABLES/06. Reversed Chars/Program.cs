d45b48bba3e605ea90d6262f0558f1279a6eef0b
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:09:13 +0200

    Update

---
+++ 7. DATA TYPES AND VARIABLES/06. Reversed Chars/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06._Reversed_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            char thirdChar = char.Parse(Console.ReadLine());
+
+            //output
+            Console.WriteLine("{0} {1} {2}",thirdChar,secondChar,firstChar);
+        }
+    }
+}
