57d08345c7899b169d9bef03092db9b47291a4e1
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:09:52 +0200

    Update

---
+++ 7. DATA TYPES AND VARIABLES/09. Chars to String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09._Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            char thirdChar = char.Parse(Console.ReadLine());
+            
+            //var           
+            string combined = firstChar.ToString()+secondChar.ToString()+thirdChar.ToString();
+
+            //output
+            Console.WriteLine(combined);
+        }
+    }
+}
