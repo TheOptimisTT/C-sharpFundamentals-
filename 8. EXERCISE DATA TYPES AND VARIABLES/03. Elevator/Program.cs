a8a3015ae0f67902aae8155595de00d60937b0a5
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:11:58 +0200

    Update

---
+++ 8. EXERCISE DATA TYPES AND VARIABLES/03. Elevator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int capacityOfElevator = int.Parse(Console.ReadLine());
+
+            //var
+            double coursesNeeded = (double)numberOfPeople / capacityOfElevator;
+
+            //Output
+            Console.WriteLine(Math.Ceiling(coursesNeeded));
+        }
+    }
+}
