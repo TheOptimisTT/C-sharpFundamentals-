bacb2ea2f5f71aaa99bb654822aa0b22eb941728
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:10:21 +0200

    Update

---
+++ 7. DATA TYPES AND VARIABLES/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int number = int.Parse(Console.ReadLine());
+
+            //var
+            int sum = 0;
+            
+            //logic
+            for (int i = 1; i <= number; i++)
+            {
+                int currentNumber = i;
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+                bool isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", currentNumber, isSpecial);
+                sum = 0;
+                i = currentNumber;
+            }
+
+        }
+    }
+}
