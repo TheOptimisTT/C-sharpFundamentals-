d7bca03467997e5867409f9443a06efa270b4f7a
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2020-12-13 22:09:25 +0200

    Update

---
+++ 7. DATA TYPES AND VARIABLES/07. Concat Names/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _07._Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            //output
+            Console.WriteLine("{0}{1}{2}",firstName,delimiter,lastName);
+        }
+    }
+}
